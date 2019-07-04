cf13425ec97fc42fa3541983475a966f77b3070f
halen33 <47633063+halen33@users.noreply.github.com>
2019-07-04 21:10:53 +0800

    Add files via upload

---
+++ pictureDeal/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace pictureDeal
+{
+    partial class Form1
+    {
+
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 77);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(212, 48);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(535, 439);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(41, 298);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(86, 49);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "放大";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(37, 189);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 45);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "灰度";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(37, 390);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(91, 45);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "边缘处理";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(841, 77);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(291, 145);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(783, 259);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(147, 50);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "启动语音识别";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(1053, 259);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(164, 44);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "关闭语音识别";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(841, 336);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(338, 28);
+            this.textBox1.TabIndex = 9;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(910, 402);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(222, 54);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "合成语音并加入数据库";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(44, 476);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(83, 30);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "注册";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1287, 546);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseWheel);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+      //  MouseWheel += new MouseEventHandler(Form1_MouseWheel);
+   //   MouseWheel += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseWheel);
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ pictureDeal/Form1.cs
@@ -0,0 +1,377 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+//using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using SpeechLib;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using MySql.Data.MySqlClient;
+using System.Drawing.Drawing2D;
+
+namespace pictureDeal
+{
+    //MouseWheel += new MouseEventHandler(Form1_MouseWheel);
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += new EventHandler(Form1_Load);
+        }
+        Bitmap bitmap;
+        //Bitmap newbitmap;
+        Stopwatch sw = new Stopwatch();
+        SpeechRecognitionEngine recEngine = new SpeechRecognitionEngine();
+        //贴图
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string path = openFileDialog1.FileName;
+                bitmap = (Bitmap)Image.FromFile(path);
+                pictureBox1.Image = bitmap.Clone() as Image;
+
+            }
+        }
+        //鼠标滚轮放大
+        private void Form1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            //判断上滑还是下滑
+            if (e.Delta < 0)
+            {
+                //计算缩放大小
+                this.pictureBox1.Width = this.pictureBox1.Width * 9 / 10;
+                this.pictureBox1.Height = this.pictureBox1.Height * 9 / 10;
+            }
+            else
+            {
+                this.pictureBox1.Width = this.pictureBox1.Width * 11 / 10;
+                this.pictureBox1.Height = this.pictureBox1.Height * 11 / 10;
+            }
+
+        }
+        //private void button2_Click(object sender, EventArgs e)
+        //{
+        //    if (bitmap != null)
+        //    {
+        //        Graphics g = this.CreateGraphics();
+        //        //g.TranslateTransform(200.0f, -100.0f);
+        //        g.DrawImage(bitmap, 200, 0, bitmap.Width / 2, bitmap.Height / 2);
+        //       // pictureBox1.Image = bitmap.Clone() as Image;
+        //        // g.FillEllipse(new SolidBrush(Color.FromArgb(50, Color.Green)), 120, 30, 200, 100);//平移
+        //    }
+        //    }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        int xPos;
+        int yPos;
+        bool MoveFlag;
+        //移动图片
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            //鼠标已经抬起
+            MoveFlag = false;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //只在鼠标按下时绘制移动
+            if (MoveFlag)
+            {
+                pictureBox1.Left += Convert.ToInt16(e.X - xPos);//设置x坐标.
+                pictureBox1.Top += Convert.ToInt16(e.Y - yPos);//设置y坐标.
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.pictureBox1.Focus();
+            MoveFlag = true;//已经按下.
+            xPos = e.X;//当前x坐标.
+            yPos = e.Y;//当前y坐标.
+        }
+
+        //灰度
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+
+            if (bitmap != null)
+            {
+                //  newbitmap = bitmap.Clone() as Bitmap;
+                sw.Reset();
+                sw.Restart();
+                Color pixel;
+                int gray;
+                for (int x = 0; x < bitmap.Width; x++)
+                {
+                    for (int y = 0; y < bitmap.Height; y++)
+                    {
+                        pixel = bitmap.GetPixel(x, y);
+                        gray = (int)(0.3 * pixel.R + 0.59 * pixel.G + 0.11 * pixel.B);
+                        bitmap.SetPixel(x, y, Color.FromArgb(gray, gray, gray));
+                    }
+                }
+                sw.Stop();
+                // timer.Text = sw.ElapsedMilliseconds.ToString();
+                pictureBox1.Image = bitmap.Clone() as Image;
+            }
+
+        }
+
+        //边缘检测算法
+        private static Bitmap smoothed(Bitmap a)
+        {
+            int w = a.Width;
+            int h = a.Height;
+            try
+            {
+                Bitmap dstBitmap = new Bitmap(w, h, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+                System.Drawing.Imaging.BitmapData srcData = a.LockBits(new Rectangle
+                    (0, 0, w, h), System.Drawing.Imaging.ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+                BitmapData dstData = dstBitmap.LockBits(new Rectangle
+                    (0, 0, w, h), System.Drawing.Imaging.ImageLockMode.WriteOnly, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+                unsafe
+                {
+                    byte* pIn = (byte*)srcData.Scan0.ToPointer();
+                    byte* pOut = (byte*)dstData.Scan0.ToPointer();
+                    byte* p;
+                    int stride = srcData.Stride;
+                    for (int y = 0; y < h; y++)
+                    {
+                        for (int x = 0; x < w; x++)
+                        {
+                            //边缘八个点像素不变
+                            if (x == 0 || x == w - 1 || y == 0 || y == h - 1)
+                            {
+                                pOut[0] = pIn[0];
+                                pOut[1] = pIn[1];
+                                pOut[2] = pIn[2];
+
+                            }
+                            else
+                            {
+                                int r0, r1, r2, r3, r4, r5, r6, r7, r8;
+                                int g1, g2, g3, g4, g5, g6, g7, g8, g0;
+                                int b1, b2, b3, b4, b5, b6, b7, b8, b0;
+                                double vR, vG, vB;
+                                //左上
+                                p = pIn - stride - 3;
+                                r1 = p[2];
+                                g1 = p[1];
+                                b1 = p[0];
+                                //正上
+                                p = pIn - stride;
+                                r2 = p[2];
+                                g2 = p[1];
+                                b2 = p[0];
+                                //右上
+                                p = pIn - stride + 3;
+                                r3 = p[2];
+                                g3 = p[1];
+                                b3 = p[0];
+                                //左
+                                p = pIn - 3;
+                                r4 = p[2];
+                                g4 = p[1];
+                                b4 = p[0];
+                                //右
+                                p = pIn + 3;
+                                r5 = p[2];
+                                g5 = p[1];
+                                b5 = p[0];
+                                //左下
+                                p = pIn + stride - 3;
+                                r6 = p[2];
+                                g6 = p[1];
+                                b6 = p[0];
+                                //正下
+                                p = pIn + stride;
+                                r7 = p[2];
+                                g7 = p[1];
+                                b7 = p[0];
+                                // 右下 
+                                p = pIn + stride + 3;
+                                r8 = p[2];
+                                g8 = p[1];
+                                b8 = p[0];
+                                //中心点
+                                p = pIn;
+                                r0 = p[2];
+                                g0 = p[1];
+                                b0 = p[0];
+                                //使用模板
+                                vR = (double)(Math.Abs(r3 + r5 + r8 - r1 - r4 - r6) + Math.Abs(r1 + r2 + r3 - r6 - r7 - r8));
+                                vG = (double)(Math.Abs(g3 + g5 + g8 - g1 - g4 - g6) + Math.Abs(g1 + g2 + g3 - g6 - g7 - g8));
+                                vB = (double)(Math.Abs(b3 + b5 + b8 - b1 - b4 - b6) + Math.Abs(b1 + b2 + b3 - b6 - b7 - b8));
+                                if (vR > 0)
+                                {
+                                    vR = Math.Min(255, vR);
+                                }
+                                else
+                                {
+                                    vR = Math.Max(0, vR);
+                                }
+
+                                if (vG > 0)
+                                {
+                                    vG = Math.Min(255, vG);
+                                }
+                                else
+                                {
+                                    vG = Math.Max(0, vG);
+                                }
+
+                                if (vB > 0)
+                                {
+                                    vB = Math.Min(255, vB);
+                                }
+                                else
+                                {
+                                    vB = Math.Max(0, vB);
+                                }
+                                pOut[0] = (byte)vB;
+                                pOut[1] = (byte)vG;
+                                pOut[2] = (byte)vR;
+
+                            }
+                            pIn += 3;
+                            pOut += 3;
+                        }
+                        pIn += srcData.Stride - w * 3;
+                        pOut += srcData.Stride - w * 3;
+
+                    }
+                }
+                a.UnlockBits(srcData);
+                dstBitmap.UnlockBits(dstData);
+
+                return dstBitmap;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        //边缘检测
+        private void button4_Click(object sender, EventArgs e)
+        {
+            bitmap = smoothed(bitmap);
+            pictureBox1.Image = bitmap.Clone() as Image;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Choices preCmd = new Choices();
+            preCmd.Add(new string[] { "天气", "你几岁啦" ,"许陈飞是猪吗","叶涵是仙女吗"});
+            GrammarBuilder gb = new GrammarBuilder();
+            gb.Append(preCmd);
+            Grammar gr = new Grammar(gb);
+            recEngine.LoadGrammarAsync(gr);
+            recEngine.SetInputToDefaultAudioDevice();
+            recEngine.SpeechRecognized += recEngine_SpeechRecognized;
+        }
+        void recEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch (e.Result.Text)
+            {
+                case "天气":
+                    richTextBox1.Text += "\n多云转晴";
+                    break;
+                case "你几岁啦":
+                    richTextBox1.Text += "\n永远18 :)";
+                    break;
+                case "许陈飞是猪吗":
+                    richTextBox1.Text += "\n是肥猪！";
+                    break;
+                case "涵涵是仙女吗":
+                    richTextBox1.Text += "\n是的呀";
+                    break;
+            }
+        }
+        //语音识别开启
+        private void button5_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsync(RecognizeMode.Multiple);
+            button5.Enabled = true;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsyncStop();
+            button6.Enabled = false;
+        }
+    
+        //语音合成
+        private void button7_Click(object sender, EventArgs e)
+        {
+            SpVoice voice = new SpVoice();
+            voice.Voice = voice.GetVoices(string.Empty, string.Empty).Item(0);
+            voice.Speak(this.textBox1.Text, SpeechVoiceSpeakFlags.SVSFDefault);
+
+            string constructorString = "server=localhost;User Id=root;password=1604010225;Database=hanhan";
+            MySqlConnection myConnnect = new MySqlConnection(constructorString);
+            myConnnect.Open();
+            MySqlCommand myCmd = new MySqlCommand("insert into infor(infor) values('" + this.textBox1.Text+"')", myConnnect);
+         //   Console.WriteLine(myCmd.CommandText);
+            if (myCmd.ExecuteNonQuery() > 0)
+            {
+                Console.WriteLine("数据插入成功！");
+            }
+
+            myCmd.Dispose();
+            myConnnect.Close();
+        }
+        void method()
+        {
+
+            // 设置APPID/AK/SK
+            var APP_ID = "16604945";
+            var API_KEY = "qTvV0EqG1Mmaavs9wIGzNBPS";                   //你的 Api Key
+            var SECRET_KEY = "wwckh4CQm2NR5tEZPu0nzNmSzg9Q3KDT";        //你的 Secret Key
+            var client = new Baidu.Aip.Face.Face(API_KEY, SECRET_KEY);
+            client.Timeout = 60000;  // 修改超时时间
+
+
+            //取决于image_type参数，传入BASE64字符串或URL字符串或FACE_TOKEN字符串
+            //你共享的图片路径（点击路径可直接查看图片）
+            var image = "http://b117.photo.store.qq.com/psb?/V10bnVD11OXPw5/QWpK4tAnWQ0G2t*V7d.MYQXiAoniZpYvqqjGn5kPoPA!/c/dHUAAAAAAAAA&bo=nAU4BJ0FOAQRECY!&rf=mood_app";
+            var imageType = "URL";
+
+
+            //注册人脸
+            var groupId = "group1";
+            var userId = "user1";
+            // 调用人脸注册，可能会抛出网络等异常，请使用try/catch捕获
+            var result = client.UserAdd(image, imageType, groupId, userId);
+            Console.WriteLine(result);
+            // 如果有可选参数
+            var options = new Dictionary<string, object>{
+                        {"user_info", "user's info"},
+                        {"quality_control", "NORMAL"},
+                        {"liveness_control", "LOW"}
+                    };
+            // 带参数调用人脸注册
+            result = client.UserAdd(image, imageType, groupId, userId, options);
+            Console.WriteLine(result);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            method();
+        }
+    }
+}
